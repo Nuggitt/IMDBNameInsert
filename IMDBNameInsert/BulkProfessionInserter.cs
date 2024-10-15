7eacea509e452ca91b966efa0f87b50992a59ff2
Nuggitt <phulip.nielsen@gmail.com>
2024-10-15 14:25:20 +0200

    Professions working

--- IMDBNameInsert/BulkProfessionInserter.cs
+++ IMDBNameInsert/BulkProfessionInserter.cs
@@ -1,6 +1,7 @@
 ﻿using IMDBNameInsert.Models;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -12,7 +13,41 @@ namespace IMDBNameInsert
     {
         public void Insert(List<Person> persons, SqlConnection sqlConn, SqlTransaction sqlTransaction)
         {
-            throw new NotImplementedException();
+            DataTable professionTable = new DataTable();
+
+            DataColumn professionID = new DataColumn("professionID", typeof(int));
+            DataColumn primaryProfession = new DataColumn("primaryProfession", typeof(string));
+
+            professionTable.Columns.Add(professionID);
+            professionTable.Columns.Add(primaryProfession);
+
+            HashSet <string> uniqueprofessions = new HashSet<string>();
+            int professionsIdCounter = 1;
+
+            foreach(Person person in persons)
+            {
+                if (!string.IsNullOrWhiteSpace(person.primaryProfession))
+                {
+                    string[] professions = person.primaryProfession.Split(',');
+
+                    foreach(string profession in professions.Select(p => p.Trim()).Distinct())
+                    {
+                        if (!uniqueprofessions.Contains(profession))
+                        {
+                            DataRow row = professionTable.NewRow();
+                            row["professionID"] = professionsIdCounter++;
+                            row["primaryProfession"] = profession;
+                            professionTable.Rows.Add(row);
+                            uniqueprofessions.Add(profession);
+
+                        }
+                    }
+                }
+            }
+
+            SqlBulkCopy bulkCopy = new SqlBulkCopy(sqlConn, SqlBulkCopyOptions.Default, sqlTransaction);
+            bulkCopy.DestinationTableName = "dbo.Professions";
+            bulkCopy.WriteToServer(professionTable);
         }
     }
 }
